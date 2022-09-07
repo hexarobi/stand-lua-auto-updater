555dfead0f3f34d19ce70e38f8fbe22aff3ae670
Tyler Chamberlain <hexarobi@gmail.com>
2022-09-07 07:38:44 -0500

    Create auto-updater.lua

---
+++ auto-updater.lua
@@ -0,0 +1,98 @@
+-- Auto-Updater v1.0
+-- by Hexarobi
+-- For Lua Scripts for the Stand Mod Menu for GTA5
+-- Example Usage:
+--    require("auto-updater")
+--    menu.action(script_meta_menu, "Check for Update", {}, "Attempt to update to latest version", function()
+--        auto_update{
+--            source_host="raw.githubusercontent.com",
+--            source_path="/hexarobi/stand-lua-hornsongs/main/HornSongs.lua",
+--            script_name=SCRIPT_NAME,
+--            script_relpath=SCRIPT_RELPATH,
+--        }
+--    end)
+
+local function string_starts(String,Start)
+    return string.sub(String,1,string.len(Start))==Start
+end
+
+local function read_version_id(auto_update_config)
+    local f = io.open(auto_update_config.version_file)
+    if f then
+        local version = f:read()
+        f:close()
+        return version
+    end
+end
+
+local function write_version_id(auto_update_config, version_id)
+    local file = io.open(auto_update_config.version_file, "wb")
+    if file == nil then
+        util.toast("Error saving version id")
+    end
+    file:write(version_id)
+    file:close()
+end
+
+local function replace_current_script(auto_update_config, new_script)
+    local file = io.open(auto_update_config.script_path, "wb")
+    if file == nil then
+        util.toast("Error updating "..auto_update_config.script_name..". Found empty script file.")
+    end
+    file:write(new_script.."\n")
+    file:close()
+end
+
+local function join_path(parent, child)
+    local sub = parent:sub(-1)
+    if sub == "/" or sub == "\\" then
+        return parent .. child
+    else
+        return parent .. "/" .. child
+    end
+end
+
+local function expand_auto_update_config(auto_update_config)
+    auto_update_config.script_clean_name = auto_update_config.script_name:gsub(".lua", "")
+    auto_update_config.script_path = filesystem.scripts_dir() .. auto_update_config.script_relpath
+    auto_update_config.script_store_dir = filesystem.store_dir() .. auto_update_config.script_clean_name .. '\\'
+    auto_update_config.version_file = join_path(auto_update_config.script_store_dir, "version.txt")
+end
+
+function auto_update(auto_update_config)
+    expand_auto_update_config(auto_update_config)
+    async_http.init(auto_update_config.source_host, auto_update_config.source_path, function(result, headers, status_code)
+        if status_code == 304 then
+            -- No update found
+            return
+        end
+        if not result or result == "" then
+            util.toast("Error updating "..auto_update_config.script_name..". Found empty script file.")
+            return
+        end
+        -- Lua scripts should begin with a comment but other HTML responses will not
+        if not string_starts(result, "--") then
+            util.toast("Error updating "..auto_update_config.script_name..". Found invalid script file.")
+            return
+        end
+        replace_current_script(auto_update_config, result)
+        if headers then
+            for header_key, header_value in pairs(headers) do
+                if header_key == "ETag" then
+                    write_version_id(auto_update_config, header_value)
+                end
+            end
+        end
+        util.toast("Updated "..auto_update_config.script_name..". Please restart script.")
+        util.stop_script()
+    end, function()
+        util.toast("Error updating "..auto_update_config.script_name..". Update failed to download.")
+    end)
+    -- Use ETags to only fetch files if they have been updated
+    -- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
+    local cached_version_id = read_version_id(auto_update_config)
+    if cached_version_id then
+        async_http.add_header("If-None-Match", cached_version_id)
+    end
+    async_http.dispatch()
+end
